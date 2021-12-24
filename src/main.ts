9b4091ceb62ceda830a41788a77bf68f81008b32
Senthil Kumar Bhaskaran <senthil.k@adappt.ai>
2021-12-24 12:43:37 +0530

    fixed the cors

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,21 @@ async function bootstrap() {
   //   },
   // });
   // app.enableCors();
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT);
 }
 
