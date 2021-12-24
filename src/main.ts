e7329cc604b670a86ce1e3c644463bdce90860cb
Senthil Kumar Bhaskaran <senthil.k@adappt.ai>
2021-12-24 12:19:05 +0530

    added multiple domain

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,20 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // the next two lines did the trick
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // var whitelist = ['example.com', 'api.example.com'];
+  const whitelist = process.env.WHITELIST_CORS.split(', ');
+  console.log(whitelist);
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   app.enableCors();
   await app.listen(process.env.PORT);
 }
